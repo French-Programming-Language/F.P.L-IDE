3ad682252e2d91e4d49ded9c4c2d7ecad740779b
Program132 <clement.gauthier132@orange.fr>
2023-04-11 16:56:50 +0200

    Icon

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
 
     WindowManager w(nullptr, "F.P.L IDE - Version 0");
+    w.setWindowIcon(QIcon("images/fpl_logo.png"));
     w.show();
 
     return QApplication::exec();
