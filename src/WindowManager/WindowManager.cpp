2c1cdd456460352935c177f0453e573d2f112f78
Program132 <clement.gauthier132@orange.fr>
2023-04-11 16:19:33 +0200

    Updated App Design + Elements

--- src/WindowManager/WindowManager.cpp
+++ src/WindowManager/WindowManager.cpp
@@ -1,21 +1,61 @@
 #include "WindowManager.h"
 
 WindowManager::WindowManager(QWidget *parent, const QString &title) : QMainWindow(parent) {
+    QString Window_StyleSheet = ""
+                                "QMainWindow {"
+                                "  background: rgb(51,54,56);"
+                                "}"
+    ;
+
     this->setWindowTitle(title);
     this->resize(600, 300);
+    this->setMaximumSize(this->size());
+    this->setStyleSheet(Window_StyleSheet);
 
-    QWidget* centralWidget = new QWidget(this);
-    this->setCentralWidget(centralWidget);
-    QLayout* existingLayout = centralWidget->layout();
+    this->widget_start = new QWidget(this);
+    this->setCentralWidget(widget_start);
+    this->existingLayout = widget_start->layout();
     if (existingLayout != nullptr) {
         delete existingLayout;
     }
 
-    this->buttonsLayout_start->addWidget(this->newFileButton_start);
-    this->buttonsLayout_start->addWidget(this->openButton_start);
+
+    auto* mainLogoLabel = new QLabel(this);
+    QPixmap mainLogo("images/fpl_logo.png");
+    QPixmap scaledImage = mainLogo.scaled(QSize(300, 100), Qt::KeepAspectRatio);
+    mainLogoLabel->setPixmap(scaledImage);
+    mainLogoLabel->setAlignment(Qt::AlignCenter);
+
+    this->mainLayout_start->addWidget(mainLogoLabel);
     this->mainLayout_start->addWidget(this->title_start);
+
+    QString Button_StyleSheet = ""
+                         "QPushButton {"
+                         "  width: 70px;"
+                         "  height: 70px;"
+                         "  border: none;"
+                         "  background: rgb(50, 92, 134);"
+                         "  color: rgb(118, 146, 183);"
+                         "  font-size: 15px;"
+                         "  text-align: center;"
+                         "  border-radius: 10px;"
+                         "}"
+                         "QPushButton:hover {"
+                         "  color: white;"
+                         "  border-radius: 15px;"
+                         "  background: rgb(50, 111, 193);"
+                         "}"
+                         ;
+
+    this->openButton_start->setStyleSheet(Button_StyleSheet);
+    this->newFileButton_start->setStyleSheet(Button_StyleSheet);
+    this->buttonsLayout_start->addItem(new QSpacerItem(0, this->height(), QSizePolicy::Expanding, QSizePolicy::Minimum));
+    this->buttonsLayout_start->addWidget(this->newFileButton_start);
+    this->buttonsLayout_start->addItem(new QSpacerItem(10, 30, QSizePolicy::Expanding, QSizePolicy::Minimum));
+    this->buttonsLayout_start->addWidget(this->openButton_start);
+    this->buttonsLayout_start->addItem(new QSpacerItem(10, 30, QSizePolicy::Expanding, QSizePolicy::Minimum));
     this->mainLayout_start->addItem(this->buttonsLayout_start);
-    centralWidget->setLayout(this->mainLayout_start);
+    widget_start->setLayout(this->mainLayout_start);
 }
 
 WindowManager::~WindowManager() = default;
--- src/WindowManager/WindowManager.h
+++ src/WindowManager/WindowManager.h
@@ -4,7 +4,11 @@
 #include <QMainWindow>
 #include <QString>
 #include <QWidget>
+#include <QPushButton>
+#include <QSpacerItem>
+#include <QPixmap>
 
+#include "../Data/Data.h"
 #include "../TextButton/TextButton.h"
 #include "../TextLabel/TextLabel.h"
 #include "../HorizontalLayout/HorizontalLayout.h"
@@ -18,12 +22,20 @@ public:
     ~WindowManager() override;
 
 private:
+    // Global
+    Data* data;
+
     // Début de l'app
+    QWidget* widget_start;
+    QLayout* existingLayout;
     VerticalLayout* mainLayout_start = new VerticalLayout(nullptr, Qt::AlignCenter);
     HorizontalLayout* buttonsLayout_start = new HorizontalLayout(nullptr, Qt::AlignCenter);
     TextLabel* title_start = new TextLabel(nullptr, "Bienvenue sur l'IDE de F.P.L", Qt::AlignCenter);
     TextButton* newFileButton_start = new TextButton(nullptr, "Nouveau");
-    TextButton* openButton_start = new TextButton(nullptr, "Ouvrir");
+    TextButton* openButton_start = new TextButton(nullptr, "Open");
+
+
+    // Editeur
 };
 
 #endif // WINDOWMANAGER_H
