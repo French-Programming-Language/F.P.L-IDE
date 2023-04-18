4f2c19ccc0418b411b829bc2dd1221fd26c80734
Program132 <clement.gauthier132@orange.fr>
2023-04-18 16:53:34 +0200

    main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
 
     WindowManager w(nullptr, "F.P.L IDE - Version 0");
-    w.setWindowIcon(QIcon("images/fpl_logo.png"));
+    w.setWindowIcon(QIcon("images/icon.png"));
     w.show();
 
     return QApplication::exec();
